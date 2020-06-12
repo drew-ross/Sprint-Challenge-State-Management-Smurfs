c9878069cc4ea3b8178a9b0b600e8033b5d1ae54
Drew Ross <mail.drew.ross@gmail.com>
2020-06-12 08:53:24 -0700

    Add redux store with thunk middleware.

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { applyMiddleware, createStore } from 'redux';
+import { rootReducer } from './reducers/index';
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(<App />, document.getElementById("root"));
